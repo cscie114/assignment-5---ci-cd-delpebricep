c7f6bede0a48ebe9b74539b6cd0cb6315952d2a6
delpebricep <pdelpebrice@gmail.com>
2023-04-20 16:25:28 -0400

    Added Netlify functions to CI pipeline

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
-exports.handler = (event, context, callback) => {
-    callback(null, {
+exports.handler = () => {
+    return {
         statusCode: 200,
         body: "Hello, World!"
-    });
+    };
 };
